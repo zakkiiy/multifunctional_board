2dd5e53d24279390982aec9302db09a18bd480c8
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-14 08:46:02 +0900

    [add]flash_messageの実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,17 +4,19 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
+    p params
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path
+      redirect_to root_path, notice: 'ログインに成功しました'
     else
-      render :new
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, notice: 'ログアウトしました', status: :see_other
   end
 end
